0ee4742b4c24e4a546459c5a1264412fa4f31add
Romeo Corpuz <nexus@romeos-mbp.gateway.ht.net>
2018-12-13 12:12:15 -1000

    built server, database and angular app with ionic and cordova

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,26 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { IonicModule } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { CoreModule } from '@app/core';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [IonicModule.forRoot(AppComponent), RouterTestingModule, TranslateModule.forRoot(), CoreModule],
+      declarations: [AppComponent],
+      providers: [StatusBar, SplashScreen]
+    });
+    TestBed.compileComponents();
+  }));
+
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+});
---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { TranslateModule } from '@ngx-translate/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { environment } from '@env/environment';
+import { CoreModule } from '@app/core';
+import { SharedModule } from '@app/shared';
+import { HomeModule } from './home/home.module';
+import { ShellModule } from './shell/shell.module';
+import { AboutModule } from './about/about.module';
+import { LoginModule } from './login/login.module';
+import { SettingsModule } from './settings/settings.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production }),
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot(),
+    IonicModule.forRoot(AppComponent, { locationStrategy: 'path' }),
+    CoreModule,
+    SharedModule,
+    ShellModule,
+    HomeModule,
+    SettingsModule,
+    AboutModule,
+    LoginModule,
+    AppRoutingModule // must be imported as the last module as it contains the fallback route
+  ],
+  declarations: [AppComponent],
+  providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }, StatusBar, SplashScreen],
+  bootstrap: [IonicApp]
+})
+export class AppModule {}
